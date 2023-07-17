d3a04fe9b8b33b91f9869b527eb315e90c38da32
Ifeoluwa-Ayantunji <ifeoluwaayantunji@gmail.com>
2023-07-17 15:05:51 +0100

    done

--- sectionjs.js
+++ sectionjs.js
@@ -246,14 +246,3 @@ auth.onAuthStateChanged(function (user){
       window.location = "home.html";
   }
 });
-
-//logout function
-
-let signOutButton = document.getElementById("signout");
-signOutButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("logout clicked");
-  auth.signOut();
-  alert("Signed out");
-  window.location = "home.html";
-}) 
